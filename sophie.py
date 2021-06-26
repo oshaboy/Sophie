056abd099813eb8c7edbc771cc845ad44a3b5568
oshaboy <noamjunk@gilor.com>
2021-06-26 21:28:41 +0300

    Readability Improvements

--- sophie.py
+++ sophie.py
@@ -2,22 +2,29 @@ import sys
 def isdigit(cand):
     return cand=="0" or cand=="1" or cand=="2" or cand=="3" or cand=="4" or cand=="5" or cand=="6" or cand=="7" or cand=="8" or cand=="9" or cand=="-"
 
-
+logfile=None
 def runstr(code):
+    global logfile
     accumulator=None
     input_buffer=""
-    loop_stack=[]
-    if_else_stack=[]
-    #global accumulator, input_buffer,loop_stack,BreakSkip
+    loop_stack=[] #the loop stack contains all the IPs after the [
+    if_else_stack=[] #the if else stack contains all the conditionals to make sure {} work correctly
     IP=0
     while(IP<len(code)):
-        codene=code[IP]
-        if (codene==" " or codene=="\n" or codene=="\t"):
+        #the codene is the char that is associated with a command
+        codene=code[IP] 
+        if (codene==" " or codene=="\n" or codene=="\t"): 
             pass
+            
+            
         elif (codene=="&"):
             return accumulator
+        
+        
         elif (codene=="*"):
+            #Pop the break stack
             loop_stack.pop()
+            #Jump to the end of the loop
             bracket_count=0
             while(code[IP]!="]"):
                 if (code[IP]=="{"):
@@ -25,32 +32,44 @@ def runstr(code):
                 elif (code[IP]=="}"):
                     bracket_count-=1
                 IP+=1
+            #Make sure you don't leave any undealt with } in the stack
             if_else_stack.pop(bracket_count)
-
+            
+            
         elif (codene=="#"):
             IP+=1
+            #If it's a number
             if (code[IP]=="$"):
                 IP+=1
                 if (code[IP]=="$"):
-                    accumulator=ord("$")
+                    accumulator=ord("$") #not sure why I put this special case here. Just use $24
+                
                 elif (isdigit(code[IP])):
+                    #Aggregate the digits
                     numstr=""
                     while (IP<len(code) and isdigit(code[IP])):
                         numstr+=code[IP]
                         IP+=1
-                    IP-=1
+                    IP-=1 #This nullifies the IP+=1 at the end of the loop
+                    #Convert it to int and plop it into the accumulator
                     accumulator=int(numstr)
                         
                 else:
                     print("Not a valid Number at {}".format(IP))
                     sys.exit(47)
-            else:
+            else: #char
                 accumulator=ord(code[IP])
+                
+                
         elif (codene=="["):
-            loop_stack.append(IP+1)
+            loop_stack.append(IP+1) 
+            
+            
         elif (codene=="]"):
             IP=loop_stack[-1]
             continue
+            
+            
         elif (codene==";"):
             try:
                 if (len(input_buffer)==0):
@@ -59,19 +78,29 @@ def runstr(code):
             
                 accumulator=ord(input_buffer[0])
                 input_buffer=input_buffer[1:]
-            except EOFError:
+            except EOFError: #0 is EOF, because why not
                 accumulator=0
+                
+                
         elif (codene==","):
             print(chr(accumulator),end="")
+            
+            
         elif (codene==":"):
             try:
                 accumulator=int(input())
             except EOFError:
                 accumulator=0
+                
+                
         elif (codene=="."):
             print(accumulator,end="")
+            
+            
+        #conditional
         elif (codene=="@"):
             IP+=1
+            #get the number to compare to (chars are converted to their Ascii/UTF-8 value)
             comparnum=None
             if (code[IP]=="$"):
                 IP+=1
@@ -87,22 +116,14 @@ def runstr(code):
             else:
                 comparnum=ord(code[IP])
                 IP+=1
+            #this bracket is required. So check for it.     
             if (code[IP]=="{"):
                 if (comparnum==accumulator):
+                    #Push a True so } will know that it might have to skip a {} clause
                     if_else_stack.append(True)
-                    """else:
-                        if(IP+1<len(code) and code[IP+1]=="{"):
-                            IP+=2
-                            bracket_count=1
-                            while(bracket_count>0):
-                                IP+=1
-                                if (code[IP]=="{"):
-                                    bracket_count+=1
-                                elif (code[IP]=="}"):
-                                    bracket_count-=1
-                                    """
                             
                 else:   
+                    #skip the {} clause
                     bracket_count=1                 
                     while(bracket_count>0):
                         IP+=1
@@ -111,18 +132,25 @@ def runstr(code):
                         elif (code[IP]=="}"):
                             bracket_count-=1
                     IP+=1
+                  
                     if (code[IP]=="{"):
+                        #Push a False so } will know that it doesn't have to skip a {} clause
                         if_else_stack.append(False)
                     else:
-                        IP-=1 #make sure it runs
+                        #nullify the IP+=1 at the end
+                        IP-=1 
                     
                     
             else:
                 print("Bracket Required after @")  
                 sys.exit(47)      
+                
+                
         elif codene=="}":
             state=if_else_stack.pop()
+            #if the previous conditional was True and there is a { ahead
             if(state and code[IP+1]=="{"):
+                #skip the clause
                 IP+=2
                 bracket_count=1
                 while(bracket_count>0):
@@ -131,7 +159,10 @@ def runstr(code):
                         bracket_count+=1
                     elif (code[IP]=="}"):
                         bracket_count-=1
+                        
+                        
         elif codene=="{": #comment
+            #just skip the clause
             bracket_count=1
             IP+=1
             while(bracket_count>0):
@@ -140,18 +171,30 @@ def runstr(code):
                     bracket_count+=1
                 elif (code[IP]=="}"):
                     bracket_count-=1
+                    
+                    
+        #debug info, puts accumulator and IP in a logfile
+        elif codene=="d":
+            if logfile==None:
+                logfile=open ("sophie.log","w")
+            logfile.write("accumulator: "+str(accumulator)+"\nIP: "+str(IP)+"\n\n")
+            
             
         else: #any other char
             print("Invalid codene {}".format( codene))
             sys.exit(47)
         IP+=1
+        
+        
     return accumulator
 
+#Return Sophie code to print a given string
 def generate_print(string):
     code=""
     for c in string:
         code+="#"+c+","
     return code
+#Return Sophie code to check for a given string
 def generate_test(string):
     code=""
     end=""
